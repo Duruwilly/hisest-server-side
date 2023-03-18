e07b2745d943c7811f3c8a803f0aaff284bd847c
princewill-broadshift <princewill.d@broadshift.com>
2023-03-18 21:23:50 +0100

    changes made

--- server.js
+++ server.js
@@ -6,34 +6,25 @@ import EmailSender from "./SendEmail.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use((req, res, next) => {
-  const allowedOrigins = [process.env.CLIENT_URL_LOCAL, process.env.CLIENT_URL_DEVELOPMENT, process.env.CLIENT_URL_PRODUCTION, '*'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
-
-//app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
-const port = process.env.PORT || 8800;
+const port = process.env.PORT || 9000;
 
 // ****** SEND API
 app.post("/send", async (req, res) => {
-  console.log(req.headers.origin);
   try {
     const { name, email, phone, subject, message } = req.body;
     EmailSender({ name, email, phone, subject, message });
-    res.json({ msg: "Message sent successfully" });
+    res.status(200).json({ msg: "Message sent successfully" });
   } catch (error) {
     res.status(404).json({ msg: "Message not sent" });
   }
 });
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`server is running on port ${port}`);
 });
